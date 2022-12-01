59c60950833dc0ca270e7eb0839241dadb0bfcce
Galochkin Daniil <galo4kin123danilo@gmail.com.com>
2022-12-01 13:33:24 +0300

    added projects section and styling

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,20 +3,58 @@ import s from './Projects.module.css';
 const Projects = () => {
   return (
     <>
-      <section className={s.wrapper}>
-        <h1 className={s.title}>Projects</h1>
-        <div className={s.project}>
-          <img
-            src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png"
-            alt=""
-            className={s.image}
-          />
-          <h2>Social Network</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-            esse reiciendis reprehenderit tenetur aspernatur est illum
-            quibusdam? Excepturi, harum ab.
-          </p>
+      <h1 className={s.title}>Projects</h1>
+      <section className={s.container}>
+        <div className={s.card}>
+          <div className={s.imgBox}>
+            <img
+              src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png"
+              alt=""
+              className={s.image}
+            />
+          </div>
+          <div className={s.content}>
+            <h2>Social Network</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate esse reiciendis reprehenderit tenetur aspernatur est
+              illum quibusdam? Excepturi, harum ab.
+            </p>
+          </div>
+        </div>
+        <div className={s.card}>
+          <div className={s.imgBox}>
+            <img
+              src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png"
+              alt=""
+              className={s.image}
+            />
+          </div>
+          <div className={s.content}>
+            <h2>Social Network</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate esse reiciendis reprehenderit tenetur aspernatur est
+              illum quibusdam? Excepturi, harum ab.
+            </p>
+          </div>
+        </div>
+        <div className={s.card}>
+          <div className={s.imgBox}>
+            <img
+              src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png"
+              alt=""
+              className={s.image}
+            />
+          </div>
+          <div className={s.content}>
+            <h2>Social Network</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate esse reiciendis reprehenderit tenetur aspernatur est
+              illum quibusdam? Excepturi, harum ab.
+            </p>
+          </div>
         </div>
       </section>
     </>
