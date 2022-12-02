a99e7c485ddc5216df2aa0df29c167b35c8d6e3f
Galochkin Daniil <galo4kin123danilo@gmail.com.com>
2022-12-02 13:56:00 +0300

    added footer+icons+links

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -2,8 +2,14 @@ const ContactForm = () => {
   return (
     <>
       <div className="container">
-        <h2>Contact form</h2>
-        <p>If you have anything to ask, offer or just to say hello=)</p>
+        <h2>
+          Contact
+          <br /> form
+        </h2>
+        <p>
+          If you have anything to ask,
+          <br /> offer or just to say hello=)
+        </p>
         <div className="row100">
           <div className="col">
             <div className="inputbox">
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,110 +1,44 @@
+import s from './Footer.module.css';
+import vk from '../../assets/vk.svg';
+import telegram from '../../assets/tg.svg';
+import codewars from '../../assets/codewars.svg';
+import github from '../../assets/github.svg';
 const Footer = () => {
   return (
-    <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <h6>About</h6>
-            <p class="text-justify">
-              Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative to
-              help the upcoming programmers with the code. Scanfcode focuses on
-              providing the most efficient code or snippets as the code wants to
-              be simple. We will help programmers build up concepts in different
-              programming languages that include C, C++, Java, HTML, CSS,
-              Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.
-            </p>
-          </div>
-
-          <div class="col-xs-6 col-md-3">
-            <h6>Categories</h6>
-            <ul class="footer-links">
-              <li>
-                <a href="http://scanfcode.com/category/c-language/">C</a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/front-end-development/">
-                  UI Design
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/back-end-development/">
-                  PHP
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/java-programming-language/">
-                  Java
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/android/">Android</a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/templates/">Templates</a>
-              </li>
-            </ul>
-          </div>
-
-          <div class="col-xs-6 col-md-3">
-            <h6>Quick Links</h6>
-            <ul class="footer-links">
-              <li>
-                <a href="http://scanfcode.com/about/">About Us</a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/contact/">Contact Us</a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/contribute-at-scanfcode/">
-                  Contribute
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/privacy-policy/">
-                  Privacy Policy
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/sitemap/">Sitemap</a>
-              </li>
-            </ul>
-          </div>
+    <footer className={s.siteFooter}>
+      <h2>Follow me</h2>
+      <div className={s.container}>
+        <div className={s.row}>
+          <a href="https://t.me/Seemslegittt" target="_blank" rel="noreferrer">
+            <img src={telegram} alt="telegram" />
+          </a>
         </div>
-        <hr />
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">
-              Copyright &copy; 2017 All Rights Reserved by
-              <a href="#">Scanfcode</a>.
-            </p>
-          </div>
-
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li>
-                <a class="facebook" href="#">
-                  <i class="fa fa-facebook"></i>
-                </a>
-              </li>
-              <li>
-                <a class="twitter" href="#">
-                  <i class="fa fa-twitter"></i>
-                </a>
-              </li>
-              <li>
-                <a class="dribbble" href="#">
-                  <i class="fa fa-dribbble"></i>
-                </a>
-              </li>
-              <li>
-                <a class="linkedin" href="#">
-                  <i class="fa fa-linkedin"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div className={s.row}>
+          <a
+            href="https://vk.com/myhighsmysobriety"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={vk} alt="vk" />
+          </a>
+        </div>
+        <div className={s.row}>
+          <a
+            href="https://github.com/Openpalms"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={github} alt="github" />
+          </a>
+        </div>
+        <div className={s.row}>
+          <a
+            href="https://www.codewars.com/users/Openpalms"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={codewars} alt="codewars" />
+          </a>
         </div>
       </div>
     </footer>
