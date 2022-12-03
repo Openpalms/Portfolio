72709df1047ae34e2ab7329a069bf9d0d5841e91
Galochkin Daniil <galo4kin123danilo@gmail.com.com>
2022-12-03 19:49:56 +0300

    new version of slider, animated and smooth

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,88 +1,12 @@
+import Slider from './NewSlider';
 import s from './Projects.module.css';
-import ImageSlider from './Slider';
 
 const Projects = () => {
-  const slides = [
-    {
-      url: 'https://res.cloudinary.com/dtpgi0zck/image/upload/s--zOSmBEhk--/c_fit,h_580,w_860/v1/EducationHub/photos/pebble-beach.jpg',
-      title: 'beach',
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1414609245224-afa02bfb3fda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80',
-      title: 'beach',
-    },
-    {
-      url: 'https://res.cloudinary.com/dtpgi0zck/image/upload/s--zOSmBEhk--/c_fit,h_580,w_860/v1/EducationHub/photos/pebble-beach.jpg',
-      title: 'beach',
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1414609245224-afa02bfb3fda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80',
-      title: 'beach',
-    },
-    {
-      url: 'https://res.cloudinary.com/dtpgi0zck/image/upload/s--zOSmBEhk--/c_fit,h_580,w_860/v1/EducationHub/photos/pebble-beach.jpg',
-      title: 'beach',
-    },
-  ];
   return (
     <>
       <h1 className={s.title}>Projects</h1>
-      {/* <section className={s.container}>
-        <div className={s.card}>
-          <div className={s.imgBox}>
-            <img
-              src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png"
-              alt=""
-              className={s.image}
-            />
-          </div>
-          <div className={s.content}>
-            <h2>Social Network</h2>
-            <p>
-              Facebook/vk copy. Build with React, redux, typescript. Styled with
-              Bootstrap 5. It's currently using 3rd party API, but soon I'll use
-              firebase for storing profiles, messages, etc. It is still in
-              progress.
-            </p>
-          </div>
-        </div>
-        <div className={s.card}>
-          <div className={s.imgBox}>
-            <img
-              src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png"
-              alt=""
-              className={s.image}
-            />
-          </div>
-          <div className={s.content}>
-            <h2>Todo list</h2>
-            <p>
-              I decided to create Todo list using some cool feautures so it will
-              not be usual simple project. So it's storing data in realtime
-              database, todos are draggable and have some more cool feautures.
-              However, styling is still in progress.
-            </p>
-          </div>
-        </div>
-        <div className={s.card}>
-          <div className={s.imgBox}>
-            <img
-              src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png"
-              alt=""
-              className={s.image}
-            />
-          </div>
-          <div className={s.content}>
-            <h2>Meetings</h2>
-            <p>
-              Tinder clone built from scratch with own server on Node.Js using
-              MongoDB to store data and register/login users.
-            </p>
-          </div>
-        </div>
-      </section> */}
       <div style={{ width: '80%', height: '600px', margin: '0 auto' }}>
-        <ImageSlider slides={slides} />
+        <Slider />
       </div>
     </>
   );
---
+++ src/components/projects/data.js
@@ -0,0 +1,39 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png',
+    name: 'Haryy Stamper',
+    title: 'manager',
+    quote:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since theic typesetting, remaining essentially unchanged. ",
+  },
+  {
+    id: 2,
+    image:
+      'https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png',
+    name: 'James Gun',
+    title: 'Project Manager',
+    quote: 'Lorem Ied. ',
+  },
+  {
+    id: 3,
+    image:
+      'https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png',
+    name: 'Michael Mosely',
+
+    title: 'Product Manager',
+    quote:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's s",
+  },
+  {
+    id: 4,
+    image:
+      'https://cdn.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png',
+    name: 'James Van',
+    title: 'Vice President',
+    quote:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's ged.",
+  },
+];
+export default people;
