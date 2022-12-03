575c1e98acc1f1f313e985f9863ec2fc6486167a
Galochkin Daniil <galo4kin123danilo@gmail.com.com>
2022-12-03 23:22:35 +0300

    Completed project

--- src/components/mainSection/Main.jsx
+++ src/components/mainSection/Main.jsx
@@ -1,14 +1,24 @@
+import CV from '../../assets/CV.pdf';
 import React from 'react';
 import video from '../../videos/video.mp4';
 import s from './Main.module.css';
+
 const Main = () => {
   return (
     <div className={s.container}>
       <span className={s.info}>
         <h1>daniil,</h1>
         <p>Frontend developer</p>
-        <button className={s.infoBtn}>Dowload resume</button>
-        <button className={s.infoBtn}>Contact me</button>
+        <button className={s.infoBtn}>
+          <a href={CV} download>
+            Dowload resume
+          </a>
+        </button>
+        <button className={s.infoBtn}>
+          <a href="https://t.me/Seemslegittt" target="_blank" rel="noreferrer">
+            Contact me
+          </a>
+        </button>
       </span>
       <div className={s.mainBlock}></div>
       <video loop autoPlay muted className={s.overlay}>
