9b0194e0ad2547f48de65d6a0014c44435d6a2d5
Galochkin Daniil <galo4kin123danilo@gmail.com.com>
2022-12-04 15:09:19 +0300

    slider refactoring

--- src/components/projects/NewSlider.jsx
+++ src/components/projects/NewSlider.jsx
@@ -43,24 +43,16 @@ const Slider = () => {
           return (
             <>
               <article className={position} key={id}>
-                <img src={image} alt={name} className="person-img" />
+                {/* <img src={image} alt={name} className="person-img" /> */}
                 <h4>{name}</h4>
                 <p className="title">{title}</p>
                 <p className="text">{quote}</p>
               </article>
-              <button
-                className="prev"
-                onClick={() => setIndex(index - 1)}
-              ></button>
-              <button
-                className="next"
-                onClick={() => setIndex(index + 1)}
-              ></button>
             </>
           );
         })}
-        {/* <button className="prev" onClick={() => setIndex(index - 1)}></button>
-        <button className="next" onClick={() => setIndex(index + 1)}></button> */}
+        <button className="prev" onClick={() => setIndex(index - 1)}></button>
+        <button className="next" onClick={() => setIndex(index + 1)}></button>
       </div>
     </section>
   );
